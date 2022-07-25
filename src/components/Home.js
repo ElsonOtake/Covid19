fc34ce67416544dd57004e8e71797d9a8d6228aa
Elson Otake <elsonotake@yahoo.com.br>
2022-07-25 15:11:33 -0300

    Update Home JSX

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,34 @@
+import S_A from '../images/S_A.png';
+import BR from '../images/BR.png';
+
 const Home = () => (
-  <h2>Home</h2>
+  <>
+    <header>
+      <p>covid in South America</p>
+    </header>
+    <main>
+      <article>
+        <img src={S_A} alt="South America map" />
+        <section>
+          <h1>South America</h1>
+          <p class="confirmed">37,672,077 Confirmed</p>
+        </section>
+      </article>
+      <section>
+        <h3>STATS BY COUNTRY</h3>
+      </section>
+      <div>
+        <article>
+          <img src={BR} alt="Brazil map" />
+          <section>
+            <h2>Brazil</h2>
+            <p>21,327,616</p>
+          </section>
+        </article>
+      </div>
+    </main>
+  </>
+  
 );
 
 export default Home;
