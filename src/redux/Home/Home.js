f53a8e5ed3d81f11cc8f0c5f5324d45c749fdabd
Elson Otake <elsonotake@yahoo.com.br>
2022-07-25 15:41:48 -0300

    Add reducer and actions

---
+++ src/redux/Home/Home.js
@@ -0,0 +1,29 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axiosGetWHO from './axiosGetWHO';
+
+const FETCH = 'who-covid19/home/FETCH';
+
+const covid19Reducer = (state = [], action = {}) => {
+  switch (action.type) {
+    case FETCH:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export const fetchWHO = createAsyncThunk(
+  FETCH,
+  async (_, { dispatch }) => {
+    const res = await axiosGetWHO()
+      .then(
+        (data) => dispatch({
+          type: FETCH,
+          payload: data,
+        }),
+      );
+    return res;
+  },
+);
+
+export default covid19Reducer;
