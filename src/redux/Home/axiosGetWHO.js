ea44da35922f41504b62732a785120cf747dd4be
Elson Otake <elsonotake@yahoo.com.br>
2022-07-25 16:03:07 -0300

    Update get without function

--- src/redux/Home/axiosGetWHO.js
+++ src/redux/Home/axiosGetWHO.js
@@ -1,29 +1,11 @@
 import axios from 'axios';
 
-const urlWHO = 'https://corona-api.com/';
-
-export const filterWHO = (countries) => {
-  const response = [];
-  countries.forEach((country) => {
-    response.push({
-      code: country.data.code,
-      name: country.data.name,
-      population: country.data.population,
-      confirmed: country.data.latest_data.confirmed,
-      critical: country.data.latest_data.critical,
-      deaths: country.data.latest_data.deaths,
-      casesPerMillion: country.data.calculated.cases_per_million_population,
-      deathRate: country.data.calculated.death_rate,
-      timeline: [],
-    });
-  });
-  return response;
-};
+const urlWHO = 'https://corona-api.com/countries';
 
 const axiosGetWHO = async () => {
   try {
     const response = await axios.get(urlWHO);
-    return filterWHO(response.data);
+    return response.data;
   } catch (error) {
     throw new Error(error);
   }
