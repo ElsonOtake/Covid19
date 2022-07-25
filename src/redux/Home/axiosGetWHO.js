37ba18f647386682737678e5d9d59eab85c4a3df
Elson Otake <elsonotake@yahoo.com.br>
2022-07-25 17:28:39 -0300

    Update filter south america countries

--- src/redux/Home/axiosGetWHO.js
+++ src/redux/Home/axiosGetWHO.js
@@ -1,11 +1,46 @@
 import axios from 'axios';
 
 const urlWHO = 'https://corona-api.com/countries';
+const southAmericaCountries = [
+  'AR',
+  'BO',
+  'BR',
+  'CL',
+  'CO',
+  'EC',
+  'GF',
+  'GY',
+  'PE',
+  'PY',
+  'SR',
+  'UY',
+  'VE',
+];
+
+export const filterWHO = (countries) => {
+  const response = [];
+  countries.data.forEach((country) => {
+    if (southAmericaCountries.includes(country.code)) {
+      response.push({
+        code: country.code,
+        name: country.name,
+        population: country.population,
+        confirmed: country.latest_data.confirmed,
+        critical: country.latest_data.critical,
+        deaths: country.latest_data.deaths,
+        casesPerMillion: country.latest_data.calculated.cases_per_million_population,
+        deathRate: country.latest_data.calculated.death_rate,
+        timeline: [],
+      });
+    }
+  });
+  return response;
+};
 
 const axiosGetWHO = async () => {
   try {
     const response = await axios.get(urlWHO);
-    return response.data;
+    return filterWHO(response.data);
   } catch (error) {
     throw new Error(error);
   }
