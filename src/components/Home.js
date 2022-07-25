83201677776fb40d024cafcb0c43d514f4ebc670
Elson Otake <elsonotake@yahoo.com.br>
2022-07-25 15:55:47 -0300

    Update dispatch and useEffect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,44 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import S_A from '../images/S_A.png';
 import BR from '../images/BR.png';
+import { fetchWHO } from '../redux/Home/Home';
 
-const Home = () => (
-  <>
-    <header>
-      <p>covid in South America</p>
-    </header>
-    <main>
-      <article>
-        <img src={S_A} alt="South America map" />
-        <section>
-          <h1>South America</h1>
-          <p class="confirmed">37,672,077 Confirmed</p>
-        </section>
-      </article>
-      <section>
-        <h3>STATS BY COUNTRY</h3>
-      </section>
-      <div>
+const Home = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(fetchWHO()));
+
+  return (
+    <>
+      <header>
+        <p>Covid19 in South America</p>
+      </header>
+      <main>
         <article>
-          <img src={BR} alt="Brazil map" />
+          <img src={S_A} alt="South America map" />
           <section>
-            <h2>Brazil</h2>
-            <p>21,327,616</p>
+            <h1>South America</h1>
+            <p className="confirmed">37,672,077 Confirmed</p>
           </section>
         </article>
-      </div>
-    </main>
-  </>
-  
-);
+        <section>
+          <h3>STATS BY COUNTRY</h3>
+        </section>
+        <div>
+          <article>
+            <img src={BR} alt="Brazil map" />
+            <section>
+              <h2>Brazil</h2>
+              <p>21,327,616</p>
+            </section>
+          </article>
+        </div>
+      </main>
+    </>
+    
+  );
+};
 
 export default Home;
