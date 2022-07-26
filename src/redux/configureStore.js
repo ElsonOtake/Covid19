662e9dafb93f1b03fcb12da946f05261ee697fdb
Elson Otake <elsonotake@yahoo.com.br>
2022-07-26 15:13:05 -0300

    Update reducer definition

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import covid19Reducer from './Home/Home';
 
-const store = configureStore({
-  reducer: covid19Reducer,
-});
+const store = configureStore({ reducer: covid19Reducer });
 
 export default store;
