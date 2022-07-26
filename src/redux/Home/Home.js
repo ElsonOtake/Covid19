1c0cfa0455bd60c3011cbddb8266b46b59df4995
Elson Otake <elsonotake@yahoo.com.br>
2022-07-26 19:37:50 -0300

    Remove comments

--- src/redux/Home/Home.js
+++ src/redux/Home/Home.js
@@ -1,5 +1,4 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-// import axios from 'axios';
 import axiosGetWHO from './axiosGetWHO';
 
 const FETCH = 'who-covid19/home/FETCH';
@@ -12,40 +11,6 @@ const covid19Reducer = (state = [], action) => {
       return state;
   }
 };
-
-// export const fetchWHO = createAsyncThunk(
-//   FETCH,
-//   async (_, { dispatch }) => {
-//     const southAmericaCountries = [
-//       'AR',
-//       'BO',
-//       'BR',
-//       'CL',
-//       'CO',
-//       'EC',
-//       'GF',
-//       'GY',
-//       'PE',
-//       'PY',
-//       'SR',
-//       'UY',
-//       'VE',
-//     ];
-//     const info = [];
-//     await Promise.all(southAmericaCountries.map((code) => {
-//       axiosGetWHO(code)
-//         .then(
-//           (data) => info.push(data),
-//         );
-//       return info;
-//     }));
-//     dispatch({
-//       type: FETCH,
-//       info,
-//     });
-//     console.log(info);
-//   },
-// );
 
 export const fetchWHO = createAsyncThunk(
   FETCH,
