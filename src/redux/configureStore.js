f3a14087b738fe7e6c2902e615d351f2345f9a17
Elson Otake <elsonotake@yahoo.com.br>
2022-07-26 19:36:08 -0300

    Add new reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import covid19Reducer from './Home/Home';
+import detailsReducer from './Details/Details';
 
-const store = configureStore({ reducer: covid19Reducer });
+const rootReducer = combineReducers({
+  covid19Reducer,
+  detailsReducer,
+});
+
+const store = configureStore({ reducer: rootReducer });
 
 export default store;
