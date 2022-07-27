8bbdf947feb26dea564eaec6fb640cee6de2c72d
Elson Otake <elsonotake@yahoo.com.br>
2022-07-26 23:25:05 -0300

    Delete App.css and index.css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Details from './components/Details';
-import './App.css';
 
 function App() {
   return (
