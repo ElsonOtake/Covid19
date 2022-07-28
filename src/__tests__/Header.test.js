a4acd0b4e2ca5298b72c91aa35217d4cb83b782e
Elson Otake <elsonotake@yahoo.com.br>
2022-07-27 22:34:23 -0300

    Add tests for Header component

---
+++ src/__tests__/Header.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from '../components/Header';
+
+describe('Tests for the Header component', () => {
+  render(
+    <Router>
+      <Header />
+    </Router>,
+  );
+  test('Check for the following test on screen', () => {
+    expect(screen.getByText(/America/)).toBeInTheDocument();
+    expect(screen.getByText('Covid19 in South America')).toBeInTheDocument();
+    expect(screen.getByRole('banner')).toBeInTheDocument();
+    expect(screen.getByRole('link')).toBeInTheDocument();
+  });
+});
