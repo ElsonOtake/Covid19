e8d845280c338f33d9392c8b30c53a4bd4638647
Elson Otake <elsonotake@yahoo.com.br>
2022-07-27 22:35:34 -0300

    Add tests for Article component

---
+++ src/__tests__/Article.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Article from '../components/Article';
+
+describe('Tests for the Article component', () => {
+  render(
+    <Router>
+      <Article />
+    </Router>,
+  );
+  test('Check for the following test on screen', () => {
+    expect(screen.getByRole('link')).toBeInTheDocument();
+    expect(screen.getByRole('article')).toBeInTheDocument();
+    expect(screen.getByRole('img')).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+  });
+});
