f2646741d54516901d0fab6216eb61e2c3fcdbd9
Elson Otake <elsonotake@yahoo.com.br>
2022-07-27 22:33:47 -0300

    Add LineCountry component

---
+++ src/charts/LineCountry.js
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import {
+  LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip,
+} from 'recharts';
+
+const LineCountry = (props) => {
+  const { title, keyData, source } = props;
+  return (
+    <section className="lineChart">
+      <h4>{title}</h4>
+      <LineChart
+        width={300}
+        height={150}
+        data={source}
+        margin={{
+          top: 5, right: 20, bottom: 5, left: 20,
+        }}
+      >
+        <Line dataKey={keyData} stroke="#ffff00" dot={false} strokeWidth={3} />
+        <CartesianGrid stroke="#fff" />
+        <XAxis dataKey="date" stroke="#fff" />
+        <YAxis stroke="#fff" />
+        <Tooltip contentStyle={{ backgroundColor: 'transparent' }} />
+      </LineChart>
+    </section>
+  );
+};
+
+const timelineShape = {
+  name: PropTypes.string,
+  newConfirmed: PropTypes.number,
+  newDeaths: PropTypes.number,
+};
+
+LineCountry.propTypes = {
+  title: PropTypes.string.isRequired,
+  keyData: PropTypes.string.isRequired,
+  source: PropTypes.objectOf(PropTypes.shape(timelineShape)).isRequired,
+};
+
+export default LineCountry;
--- src/components/Details.js
+++ src/components/Details.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import {
-  LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, PieChart, Pie, Cell,
+  PieChart, Pie, Cell,
 } from 'recharts';
 import { fetchDetails } from '../redux/Details/Details';
 import S_A from '../images/S_A.png';
@@ -19,6 +19,7 @@ import PY from '../images/PY.png';
 import SR from '../images/SR.png';
 import UY from '../images/UY.png';
 import VE from '../images/VE.png';
+import LineCountry from '../charts/LineCountry';
 
 const Details = () => {
   const detailsData = useSelector((state) => state.detailsReducer);
@@ -118,48 +119,38 @@ const Details = () => {
 
   const COLORS = ['#21618C', '#2E86C1'];
 
+  const textPerc = (perc, cx, cy) => (
+    <text x={cx} y={cy} dy={8} textAnchor="middle" fontSize={25} fill="#ffff00">
+      {`${parseFloat(perc).toFixed(0)}%`}
+    </text>
+  );
+
   const renderCustomizedLabelPopulation = ({ cx, cy }) => {
     const perc = 100 * (piePopulationData[0].value
       / (piePopulationData[0].value + piePopulationData[1].value));
 
-    return (
-      <text x={cx} y={cy} dy={8} textAnchor="middle" fontSize={25} fill="#ffff00">
-        {`${parseFloat(perc).toFixed(0)}%`}
-      </text>
-    );
+    return textPerc(perc, cx, cy);
   };
 
   const renderCustomizedLabelConfirmed = ({ cx, cy }) => {
     const perc = 100 * (pieConfirmedData[0].value
       / (pieConfirmedData[0].value + pieConfirmedData[1].value));
 
-    return (
-      <text x={cx} y={cy} dy={8} textAnchor="middle" fontSize={25} fill="#ffff00">
-        {`${parseFloat(perc).toFixed(0)}%`}
-      </text>
-    );
+    return textPerc(perc, cx, cy);
   };
 
   const renderCustomizedLabelCritical = ({ cx, cy }) => {
     const perc = 100 * (pieCriticalData[0].value
       / (pieCriticalData[0].value + pieCriticalData[1].value));
 
-    return (
-      <text x={cx} y={cy} dy={8} textAnchor="middle" fontSize={25} fill="#ffff00">
-        {`${parseFloat(perc).toFixed(0)}%`}
-      </text>
-    );
+    return textPerc(perc, cx, cy);
   };
 
   const renderCustomizedLabelDeaths = ({ cx, cy }) => {
     const perc = 100 * (pieDeathsData[0].value
       / (pieDeathsData[0].value + pieDeathsData[1].value));
 
-    return (
-      <text x={cx} y={cy} dy={8} textAnchor="middle" fontSize={25} fill="#ffff00">
-        {`${parseFloat(perc).toFixed(0)}%`}
-      </text>
-    );
+    return textPerc(perc, cx, cy);
   };
 
   return (
@@ -171,7 +162,7 @@ const Details = () => {
               <img src={imgSrc} alt={`${name} map`} />
               <section>
                 <h1>{name}</h1>
-                <p className="confirmed">{`${confirmed.toLocaleString()} Confirmed`}</p>
+                <p className="confirmed">{`${confirmed?.toLocaleString() || 0} Confirmed`}</p>
               </section>
             </article>
             <section>
@@ -180,64 +171,32 @@ const Details = () => {
             <section className="statistics">
               <div className="population">
                 <div>
-                  <p className="number">{population.toLocaleString()}</p>
+                  <p className="number">{population?.toLocaleString() || 0}</p>
                   <p className="text">population</p>
                 </div>
                 <div>
-                  <p className="number">{casesPerMillion.toLocaleString()}</p>
+                  <p className="number">{casesPerMillion?.toLocaleString() || 0}</p>
                   <p className="text">cases / million</p>
                 </div>
                 <div>
-                  <p className="number">{deathRate.toFixed(2)}</p>
+                  <p className="number">{deathRate?.toFixed(2) || 0}</p>
                   <p className="text">death rate</p>
                 </div>
               </div>
               <div className="critical">
                 <div>
-                  <p className="number">{critical.toLocaleString()}</p>
+                  <p className="number">{critical?.toLocaleString() || 0}</p>
                   <p className="text">critical</p>
                 </div>
                 <div>
-                  <p className="number">{deaths.toLocaleString()}</p>
+                  <p className="number">{deaths?.toLocaleString() || 0}</p>
                   <p className="text">deaths</p>
                 </div>
               </div>
             </section>
             <section className="line">
-              <section className="lineChart">
-                <h4>Deaths</h4>
-                <LineChart
-                  width={300}
-                  height={150}
-                  data={timeline}
-                  margin={{
-                    top: 5, right: 20, bottom: 5, left: 20,
-                  }}
-                >
-                  <Line dataKey="newDeaths" stroke="#ffff00" dot={false} strokeWidth={3} />
-                  <CartesianGrid stroke="#fff" />
-                  <XAxis dataKey="date" stroke="#fff" />
-                  <YAxis stroke="#fff" />
-                  <Tooltip contentStyle={{ backgroundColor: 'transparent' }} />
-                </LineChart>
-              </section>
-              <section className="lineChart">
-                <h4>Confirmed</h4>
-                <LineChart
-                  width={300}
-                  height={150}
-                  data={timeline}
-                  margin={{
-                    top: 5, right: 20, bottom: 5, left: 20,
-                  }}
-                >
-                  <Line dataKey="newConfirmed" stroke="#ffff00" dot={false} strokeWidth={3} />
-                  <CartesianGrid stroke="#fff" />
-                  <XAxis dataKey="date" stroke="#fff" />
-                  <YAxis stroke="#fff" />
-                  <Tooltip contentStyle={{ backgroundColor: 'transparent' }} />
-                </LineChart>
-              </section>
+              <LineCountry title="Deaths" keyData="newDeaths" source={timeline} />
+              <LineCountry title="Confirmed" keyData="newConfirmed" source={timeline} />
             </section>
             <h5>South America</h5>
             <div className="pie">
