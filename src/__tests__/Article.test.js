20d10c028dfd2cd0b3d391bed141490ee1584dc6
Elson Otake <elsonotake@yahoo.com.br>
2022-07-28 11:06:31 -0300

    Update tests for Article component

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -12,8 +12,15 @@ describe('Tests for the Article component', () => {
   );
   test('Check for the following test on screen', () => {
     expect(screen.getByRole('link')).toBeInTheDocument();
+    expect(screen.getByRole('link')).toBeVisible();
     expect(screen.getByRole('article')).toBeInTheDocument();
+    expect(screen.getByRole('article')).toBeVisible();
     expect(screen.getByRole('img')).toBeInTheDocument();
+    expect(screen.getByRole('img')).toBeVisible();
     expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toBeVisible();
+  });
+  test('Check for the snapshot', () => {
+    expect(screen.debug()).toMatchSnapshot();
   });
 });
